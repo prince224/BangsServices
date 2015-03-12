87b99657bd809eb13b29eb305b0b1ff1881084a9
prince224 <nfansou.bangoura@gmail.com>
2015-03-12 11:35:34 +0100

    create function add, update menu and remove pages from menu

--- src/Cms/DomaineBundle/Controller/AdminController.php
+++ src/Cms/DomaineBundle/Controller/AdminController.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+    @Author Prince Bangoura
+    @product CMS build with PHP >=5.3.3 SYMFONY 2.5 - Bootstrap 3.3.2 - ckeditor 4.4.7
+    @Function Engineer
+    @Young entrepreneur
+    @Date==>2015
+    @V 0.1
+*/
 namespace Cms\DomaineBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -7,6 +14,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Cms\DomaineBundle\Entity\Photo;
 use Cms\DomaineBundle\Form\PhotoType;
+
+use Cms\DomaineBundle\Entity\Menu;
+use Cms\DomaineBundle\Form\MenuType;
 
 use Cms\DomaineBundle\Entity\SousMenu;
 use Cms\DomaineBundle\Form\SousMenuType;
@@ -23,4 +33,108 @@ class AdminController extends Controller
         return $this->render('DomaineBundle:Admin:index.html.twig');
     }
 
+    /*===================menu homepage ========================== */
+    public function menu_homepageAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+
+        $menus = $em->getRepository('DomaineBundle:Menu')->findAll();
+        $pages = $em->getRepository('PageBundle:Page')->findAll();
+
+        return $this->render('DomaineBundle:Admin:menu_homepage.html.twig',array(
+            'menus' => $menus,
+            'pages' => $pages,
+            ));
+    }
+    /*===============Fin menu homepage===================*/
+
+    /*===================ajouter menu ========================== */
+    public function ajouter_menuAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+
+        $menu = new Menu();
+
+        if($request->getMethod() == 'POST')
+        {
+            $choix_pages = $_POST['choix'];
+            if($choix_pages != null)
+            {
+                foreach ($choix_pages as $idChoix_page) {
+                    $page = $em->getRepository('PageBundle:Page')->find($idChoix_page);
+                    if($page != null)
+                    {
+                        $menu->addPage($page);
+                        $menu->setNom('BS_menu');
+                        $em->persist($menu);
+                    }
+                }//end for
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('domaine_admin_menu_homepage'));
+            }
+            
+        }
+
+        return $this->redirect($this->generateUrl('domaine_admin_menu_homepage'));
+    }
+    /*===============Fin ajouter menu===================*/
+
+    /*===================modifier_nom menu ========================== */
+    public function modifier_nom_menuAction($idmenu)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+
+        $menu = $em->getRepository('DomaineBundle:Menu')->find($idmenu);
+
+        $form = $this->createForm(new MenuType, $menu);
+
+        if($request->getMethod() == 'POST')
+        {
+            $form->bind($request);
+            if($form->isValid())
+            {
+                $menu= $form->getData();
+
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('domaine_admin_menu_homepage'));
+            }
+            
+        }
+
+        return $this->render('DomaineBundle:Admin:modifier_nom_menu.html.twig', array(
+            'form' =>$form->createView(),
+            ));
+    }
+    /*===============Fin modifier_nom menu===================*/
+
+    /*===================supprimer_page menu ========================== */
+    public function supprimer_page_menuAction($idmenu)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+
+        $menu = $em->getRepository('DomaineBundle:Menu')->find($idmenu);
+
+        $idpage = $request->query->get('page');
+        $page = $em->getRepository('PageBundle:Page')->find($idpage);
+
+      
+        if($menu != null and $page != null)
+        {
+            $menu->removePage($page);
+
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('domaine_admin_menu_homepage'));
+    
+        }
+
+        return $this->redirect($this->generateUrl('domaine_admin_menu_homepage'));
+    }
+    /*===============Fin supprimer_page menu===================*/
 }   
---
+++ src/Cms/DomaineBundle/Form/MenuType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Cms\DomaineBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MenuType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            //->add('pages')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Cms\DomaineBundle\Entity\Menu'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'cms_domainebundle_menu';
+    }
+}
