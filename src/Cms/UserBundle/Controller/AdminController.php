b496215aa318991032f6c9e5476caa2ac3cc7fdf
prince224 <nfansou.bangoura@gmail.com>
2015-03-27 10:23:56 +0100

    create function update user

--- src/Cms/UserBundle/Controller/AdminController.php
+++ src/Cms/UserBundle/Controller/AdminController.php
@@ -10,7 +10,21 @@
 namespace Cms\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\Event\UserEvent;
 
 use Cms\UserBundle\Entity\User;
 use Cms\UserBundle\Form\UserType;
@@ -27,4 +41,54 @@ class AdminController extends Controller
             'utilisateurs' => $utilisateurs,
             ));
     }
+    /*========Fin homepage_utilisateur =============================== */
+
+    /*================= modifier utilisateur ==========================*/
+    public function modifier_utilisateurAction(Request $request, $idutilisateur)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+     
+        $user = $em->getRepository('UserBundle:User')->find($idutilisateur);
+        $user->setEnabled(true);
+        
+
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, new UserEvent($user, $request));
+     
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+     
+        if ('POST' === $request->getMethod()) {
+
+            $form->bind($request);
+
+     
+            if ($form->isValid()) {
+
+                $userprofil = $user->getProfil();
+
+                $role = array($userprofil);
+
+                $user->setRoles($role);
+
+                $userManager->updateUser($user);
+            
+                return $this->redirect($this->generateUrl('user_homepage'));
+                //return $response;
+            }
+        }
+     
+        return $this->render('UserBundle:Admin:modifier_utilisateur.html.twig', array(
+        'form' => $form->createView(),
+        'utilisateur' => $user,
+        ));
+    }
+    /*==============Fin modifier utilisateur ==========================*/
+
 }
